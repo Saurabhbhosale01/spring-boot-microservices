f64f148bf9daaa02edcffdc705fe28f6192a1d41
Saurabh Bhosale <saurabhbhosale426@gmail.com>
2026-06-20 12:10:42 +0530

    .

--- product-service/src/main/java/com/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/product_service/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.product_service.controller;
 
 import com.product_service.dto.Product;
 import com.product_service.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,31 +17,34 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "create product")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void saveProduct(@RequestBody Product product){
          productService.saveProduct(product);
     }
 
+    @Operation(summary = "get all  product")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Product> getAllProduct(){
         return productService.getAllProduct();
     }
-
+    @Operation(summary = "get product by id")
     @GetMapping("/{productId}")
     @ResponseStatus(HttpStatus.OK)
     public Product getProductById(@PathVariable UUID productId){
        return productService.getProductById(productId);
     }
 
-
+    @Operation(summary = "update product by id")
     @PutMapping("/{productId}")
     @ResponseStatus(HttpStatus.OK)
     public void updateProduct(@RequestBody Product product, @PathVariable UUID productId){
         productService.updateProduct(product, productId);
     }
 
+    @Operation(summary = "delete(soft) product")
     @PutMapping("/{productId}/{status}")
     @ResponseStatus(HttpStatus.OK)
     public void archiveProduct(@PathVariable UUID productId, @PathVariable boolean status){
